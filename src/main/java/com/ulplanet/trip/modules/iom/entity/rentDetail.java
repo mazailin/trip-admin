7e05fe45d32fc16b0f1a51c5bbbf2a67e387e665
zhangxd1989 <zhangxd_2011sz@163.com>
2015-12-18 15:20:51 +0800

    租赁明细

---
+++ src/main/java/com/ulplanet/trip/modules/iom/entity/rentDetail.java
@@ -0,0 +1,79 @@
+package com.ulplanet.trip.modules.iom.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ulplanet.trip.common.persistence.DataEntity;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * 产品Entity
+ * Created by zhangxd on 15/12/01.
+ */
+public class rentDetail extends DataEntity<rentDetail> {
+
+	private static final long serialVersionUID = 1L;
+
+    private String code;
+    private Product product;
+    private ProductDetail productDetail;
+    private double amount;
+    private String reasons;
+    private Date disDate;
+
+    public rentDetail() {
+        super();
+    }
+
+    @NotNull
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public ProductDetail getProductDetail() {
+        return productDetail;
+    }
+
+    public void setProductDetail(ProductDetail productDetail) {
+        this.productDetail = productDetail;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Length(min=0, max=2000, message="报废原因长度必须介于 1 和 2000 之间")
+    public String getReasons() {
+        return reasons;
+    }
+
+    public void setReasons(String reasons) {
+        this.reasons = reasons;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getDisDate() {
+        return disDate;
+    }
+
+    public void setDisDate(Date disDate) {
+        this.disDate = disDate;
+    }
+}
