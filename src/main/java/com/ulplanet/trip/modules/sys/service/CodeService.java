bebe33f7b45d38a03f35e5b64b149e6fa6295bb7
zhangxd1989 <zhangxd_2011sz@163.com>
2016-02-01 10:31:02 +0800

    库存管理调整

--- src/main/java/com/ulplanet/trip/modules/sys/service/CodeService.java
+++ src/main/java/com/ulplanet/trip/modules/sys/service/CodeService.java
@@ -24,7 +24,6 @@ public class CodeService extends CrudService<CodeDao, Code> {
     public static final String CODE_TYPE_PRODUCT_IN         = "3";
     public static final String CODE_TYPE_PRODUCT_DISCARD    = "4";
     public static final String CODE_TYPE_PRODUCT_RENT       = "5";
-    public static final String CODE_TYPE_PRODUCT_RETURN     = "6";
 
     @Autowired
     private CodeDao codeDao;
